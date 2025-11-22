533230225739319e724feb750ec284507deef7f0
Jose Manuel Poot <jpoot@kensium.com>
2025-11-22 12:26:01 -0600

    Initial project blazor

---
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,13 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.Components.Pages
+{
+    public partial class EmployeeOverview
+    {  public List<Employee>? Employees { get; set; }= default!;
+        protected async override Task OnInitializedAsync()
+        {
+           Employees = MockDataService.Employees;
+        }
+    }
+}
