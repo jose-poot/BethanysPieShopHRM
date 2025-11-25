c8059ea27b1c3eed051b2f994ee0d2b7737bd81e
Jose Manuel Poot <jpoot@kensium.com>
2025-11-24 18:53:20 -0600

    feat: Wire up QuickViewPopup in EmployeeOverview page

--- BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -4,10 +4,20 @@ using BethanysPieShopHRM.Shared.Domain;
 namespace BethanysPieShopHRM.Components.Pages
 {
     public partial class EmployeeOverview
-    {  public List<Employee>? Employees { get; set; }= default!;
+    {
+        private Employee? _selectedEmployee;
+
+        public List<Employee>? Employees { get; set; }= default!;
         protected async override Task OnInitializedAsync()
         {
-           Employees = MockDataService.Employees;
+           /* await Task.Delay(200);*/ // Simulate async data fetch
+            Employees = MockDataService.Employees;
+        }
+
+        public void ShowQuickViewPopup(Employee selectedEmployee)
+        {
+            _selectedEmployee = selectedEmployee;
+           
         }
     }
 }
