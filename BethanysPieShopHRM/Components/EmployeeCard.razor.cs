753aa927a2fe6ed085c20adbc1e4dfe623f3ea1b
Jose Manuel Poot <jpoot@kensium.com>
2025-11-24 21:09:35 -0600

    feat: Implement EmployeeCard validation and ErrorBoundary

--- BethanysPieShopHRM/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM/Components/EmployeeCard.razor.cs
@@ -10,5 +10,14 @@ namespace BethanysPieShopHRM.Components
 
         [Parameter]
         public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (string.IsNullOrEmpty(Employee.LastName))
+            {
+
+                throw new Exception("Last name can't be empty");
+            }
+        }
     }
 }
