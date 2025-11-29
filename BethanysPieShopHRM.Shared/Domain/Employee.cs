c8a12c9cd62e6547607f624d6f60eb8df743de31
Jose Manuel Poot <jpoot@kensium.com>
2025-11-29 17:47:17 -0600

    feat: Add data validation annotations to Employee model

--- BethanysPieShopHRM.Shared/Domain/Employee.cs
+++ BethanysPieShopHRM.Shared/Domain/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BethanysPieShopHRM.Shared.Domain
 {
@@ -6,12 +7,18 @@ namespace BethanysPieShopHRM.Shared.Domain
     {
         public int EmployeeId { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = "First name is too long.")]
         public string FirstName { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name is too long.")]
         public string LastName { get; set; } = string.Empty;
 
         public DateTime? BirthDate { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
         public string? Street { get; set; }
@@ -34,6 +41,7 @@ namespace BethanysPieShopHRM.Shared.Domain
 
         public bool IsOnHoliday { get; set; } = false;
 
+        [StringLength(1000, ErrorMessage = "Comment length can't exceed 1000 characters.")]
 
         public string Comment { get; set; } = string.Empty;
 
